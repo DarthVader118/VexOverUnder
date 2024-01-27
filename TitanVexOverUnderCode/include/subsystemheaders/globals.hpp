30970432a4fd70cba2fcc6a7ad46fb0db28e9e58
Akshit Kumar <akshit2k26@gmail.com>
2024-01-27 14:44:45 -0500

    Update globals.hpp

--- TitanVexOverUnderCode/include/subsystemheaders/globals.hpp
+++ TitanVexOverUnderCode/include/subsystemheaders/globals.hpp
@@ -6,4 +6,6 @@ extern pros::Motor driveLeftBack;
 extern pros::Motor driveLeftFront;
 extern pros::Motor driveRightBack;
 extern pros::Motor driveRightFront;
+extern pros::Motor launcherMotor;
+
 extern pros::Controller controller;
