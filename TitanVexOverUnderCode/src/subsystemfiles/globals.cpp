68c03a14adde5b0bb8f7e33dafd2c73923ab4dbf
goel-raghav <63321893+DarthVader118@users.noreply.github.com>
2024-01-27 13:17:54 -0500

    controller

--- TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
+++ TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
@@ -5,3 +5,5 @@ pros::Motor driveLeftBack(2);
 pros::Motor driveLeftFront(3);
 pros::Motor driveRightBack(4);
 pros::Motor driveRightFront(5);
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
