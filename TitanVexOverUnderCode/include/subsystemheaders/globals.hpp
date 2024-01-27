96499b76858e2671ad266eb6274965ca3ca866ae
goel-raghav <63321893+DarthVader118@users.noreply.github.com>
2024-01-27 13:16:30 -0500

    defined drive motors and contoller

--- TitanVexOverUnderCode/include/subsystemheaders/globals.hpp
+++ TitanVexOverUnderCode/include/subsystemheaders/globals.hpp
@@ -2,3 +2,8 @@
 
 // Motors
 extern pros::Motor intake;
+extern pros::Motor driveLeftBack;
+extern pros::Motor driveLeftFront;
+extern pros::Motor driveRightBack;
+extern pros::Motor driveRightFront;
+extern pros::Controller controller;
--- TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
+++ TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
@@ -1,3 +1,7 @@
 #include "main.h"
 
 pros::Motor intake(1);
+pros::Motor driveLeftBack(2);
+pros::Motor driveLeftFront(3);
+pros::Motor driveRightBack(4);
+pros::Motor driveRightFront(5);
