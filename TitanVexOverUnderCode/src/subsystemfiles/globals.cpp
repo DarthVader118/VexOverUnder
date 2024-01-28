34450359ce4de726977c88c647265adeace1b1d7
Arjun Bakhale <109678629+ArjunBakhale@users.noreply.github.com>
2024-01-28 15:15:18 -0500

    Update globals.cpp
    fsadfdasf

--- TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
+++ TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
@@ -1,12 +1,17 @@
 #include "main.h"
 
+
+#define FLAP_LEFT_PORT 7  // Replace 1 with the actual port number
+#define FLAP_RIGHT_PORT 8 // Replace 2 with the actual port number
+
+
 pros::Motor intake(1);
 pros::Motor driveLeftBack(2);
 pros::Motor driveLeftFront(3);
 pros::Motor driveRightBack(4);
 pros::Motor driveRightFront(5);
 
-pros::Motor flapLeftMotor(6); // Replace 6 with the actual port number
-pros::Motor flapRightMotor(7); // Replace 7 with the actual port number
+pros::ADIDigitalOut leftPiston (FLAP_LEFT_PORT);
+pros::ADIDigitalOut rightPiston (FLAP_RIGHT_PORT);
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
