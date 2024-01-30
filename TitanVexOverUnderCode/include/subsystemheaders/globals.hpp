c168be2376058d3c1daf44737aa06f31c7fb340f
ArjunBakhale <875745@lcps.org>
2024-01-30 18:43:24 -0500

    fix globals

--- TitanVexOverUnderCode/include/subsystemheaders/globals.hpp
+++ TitanVexOverUnderCode/include/subsystemheaders/globals.hpp
@@ -12,5 +12,10 @@ extern pros::Motor launcherMotor;
 extern pros::ADIDigitalOut leftPiston;
 extern pros::ADIDigitalOut rightPiston;
 
+// Motor Groups
+extern pros::Motor_Group leftDrive;
+extern pros::Motor_Group rightDrive;
+
+
 
 extern pros::Controller controller;
