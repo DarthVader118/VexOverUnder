4ba486263a47809df1225d25f3ee99e0a9644c67
Akshit Kumar <akshit2k26@gmail.com>
2024-01-30 20:22:38 -0500

    qwer

--- TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
+++ TitanVexOverUnderCode/src/subsystemfiles/globals.cpp
@@ -11,15 +11,7 @@ pros::Motor driveLeftFront(3);
 pros::Motor driveRightBack(4);
 pros::Motor driveRightFront(5);
 
-<<<<<<< HEAD
-pros::Motor_Group leftDrive(pros:: Motor driveLeftFront, pros::Motor driveLeftBack);
-pros::Motor_Group rightDrive(pros:: Motor driveRightFront, pros::Motor driveRightBack);
-
-pros::Motor flapLeftMotor(6); // Replace 6 with the actual port number
-pros::Motor flapRightMotor(7); // Replace 7 with the actual port number
-=======
 pros::ADIDigitalOut leftPiston (FLAP_LEFT_PORT);
 pros::ADIDigitalOut rightPiston (FLAP_RIGHT_PORT);
->>>>>>> ee5780cd26519cddb468cdc81e26b16dead47fe9
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
