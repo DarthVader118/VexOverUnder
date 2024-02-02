9239b48a4e90e7fc32a97037338de1ed588120f9
Akshit Kumar <akshit2k26@gmail.com>
2024-02-01 19:23:08 -0500

    added auton methods (real)

--- TitanVexOverUnderCode/include/subsystemheaders/autonomous.hpp
+++ TitanVexOverUnderCode/include/subsystemheaders/autonomous.hpp
@@ -1,3 +1,9 @@
 #include "main.h"
 
 void autonomous();
+
+void driveAuton(int units);
+
+void turnLeft(int units);
+
+void turnRight(int units);
--- TitanVexOverUnderCode/src/autonomous.cpp
+++ TitanVexOverUnderCode/src/autonomous.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "subsystemfiles\globals.cpp"
 
 // void redLeftCorner(){
 //     //move(100, 100)(something, something)
@@ -31,3 +32,30 @@
 //     blueLeftCorner();
 //     blueRightCorner();
 // }
+
+#define MOTOR_MAX_SPEED 100
+
+void driveAuton(int units){
+    /*driveLeftBack.move_relative(units, MOTOR_MAX_SPEED);
+    driveRightBack.move_relative(units, MOTOR_MAX_SPEED);
+    driveLeftFront.move_relative(units, MOTOR_MAX_SPEED);
+    driveRightFront.move_relative(units, MOTOR_MAX_SPEED);*/
+    leftDrive.move_relative(units, MOTOR_MAX_SPEED);
+    rightDrive.move_relative(units, MOTOR_MAX_SPEED);
+}
+
+void turnLeft(int units){
+    rightDrive.move_relative(units, MOTOR_MAX_SPEED);
+    leftDrive.move_relative(-units, MOTOR_MAX_SPEED);
+}
+
+void turnRight(int units){
+    rightDrive.move_relative(-units, MOTOR_MAX_SPEED);
+    leftDrive.move_relative(units, MOTOR_MAX_SPEED);
+}
+
+void runLauncher(){
+    launcherMotor = -127; // Replace 127 with the speed you want
+    pros::delay(1100); // Wait for a fixed time (1 second in this case)
+    launcherMotor = 0; // Stop the launcher
+}
